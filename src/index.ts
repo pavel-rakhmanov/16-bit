5bc574ca1a98cb4874b3ed48d204df070d0b10cc
pavel.rakhmanov <pavel.rakhmanov@datart.com>
2021-03-31 19:53:01 +0300

    add cpu step on readline line event

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,23 @@ import { CPUEntity } from './domains/entities/cpu.entity';
 
 import propgramm from './programm';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const readline = require('readline');
+
 const cpu = new CPUEntity(propgramm);
 
-console.log(`- [initial] step --------------------------------`)
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 cpu.debug();
 cpu.viewMemoryAt(cpu.getRegister(Register.IP))
 cpu.viewMemoryAt(0x0100);
 
-for (let i = 0; i < 20; i += 1) {
-  console.log(`- [${i + 1}] step ------------------------------------`)
-
+rl.on('line', () => {
   cpu.step();
   cpu.debug();
   cpu.viewMemoryAt(cpu.getRegister(Register.IP))
   cpu.viewMemoryAt(0x0100);
-}
+})
